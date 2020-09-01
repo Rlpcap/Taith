e131cb50c6ea9a642cf0e8b56119b57d39f66955
Rlpcap <rafael.lopezpisani@davinci.edu.ar>
2020-08-31 21:28:22 -0300

    Movimiento de la camara, y detalles de gravedad en la escena

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour, IUpdate
+{
+    public GameObject target;
+    public float dampSpeed;
+    float zDistance;
+    void Start()
+    {
+        UpdateManager.Instance.AddElementUpdate(this);
+        zDistance = (target.transform.position - transform.position).z;
+    }
+
+    public void OnUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        var nextPos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z - zDistance);
+        transform.position = Vector3.Lerp(transform.position, nextPos, dampSpeed);
+    }
+}
