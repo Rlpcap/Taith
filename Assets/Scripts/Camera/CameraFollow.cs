c870ad54cbd17605c05520c2f56f5c2fa92dee21
julianbernabeu <julian.bernabeu@davinci.edu.ar>
2020-09-16 17:23:52 -0300

    Añadido LateUpdate a updatemanager

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour, IUpdate
+public class CameraFollow : MonoBehaviour, ILateUpdate
 {
     public GameObject target;
     public float dampSpeed;
     float zDistance;
     void Start()
     {
-        UpdateManager.Instance.AddElementUpdate(this);
+        UpdateManager.Instance.AddElementLateUpdate(this);
         zDistance = (target.transform.position - transform.position).z;
     }
 
-    public void OnUpdate()
+
+    public void OnLateUpdate()
     {
         Move();
     }
@@ -23,4 +24,5 @@ public class CameraFollow : MonoBehaviour, IUpdate
         var nextPos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z - zDistance);
         transform.position = Vector3.Lerp(transform.position, nextPos, dampSpeed);
     }
+
 }
