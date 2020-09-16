53ec1bfa44e5114db4e0c59f65f3adc519d999b3
Rlpcap <rafael.lopezpisani@davinci.edu.ar>
2020-09-16 17:35:40 -0300

    Puzzles 2 y 3 mios agregados. La cámara ahora también te sigue en Y.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -7,20 +7,24 @@ public class CameraFollow : MonoBehaviour, IUpdate
     public GameObject target;
     public float dampSpeed;
     float zDistance;
+    Vector3 dist;
     void Start()
     {
         UpdateManager.Instance.AddElementUpdate(this);
         zDistance = (target.transform.position - transform.position).z;
+        dist = target.transform.position - transform.position;
     }
 
     public void OnUpdate()
     {
         Move();
-    }
+    }    
 
     void Move()
     {
-        var nextPos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z - zDistance);
+        //var nextPos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z - zDistance);
+        var nextPos = new Vector3(target.transform.position.x, target.transform.position.y - dist.y, target.transform.position.z - dist.z);
+
         transform.position = Vector3.Lerp(transform.position, nextPos, dampSpeed);
     }
 }
