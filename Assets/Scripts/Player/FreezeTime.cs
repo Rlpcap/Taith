8b14bec284ad0704bc549d3253edf9db2a39add9
julianbernabeu <julian.bernabeu@davinci.edu.ar>
2020-09-27 18:14:07 -0300

    Interfaz power y script de freezetime

---
+++ Assets/Scripts/Player/FreezeTime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeTime : MonoBehaviour ,IPower
+{
+    PlayerModel _player;
+    float _timeStopRange;
+    LayerMask _groundLayer;
+    float _freezeTime;
+
+    public FreezeTime(PlayerModel player,float timeStopRange, LayerMask groundLayer, float freezeTime)
+    {
+        _player = player;
+        _timeStopRange = timeStopRange;
+        _groundLayer = groundLayer;
+        _freezeTime = freezeTime;
+    }
+
+    public void UsePower()
+    {
+        var propsInArea = Physics.OverlapSphere(_player.transform.position, _timeStopRange, _groundLayer);
+        foreach (var prop in propsInArea)
+        {
+            if (prop.GetComponent<IFreezable>() != null)
+                StartCoroutine(prop.GetComponent<IFreezable>().FreezeTime(_freezeTime));
+        }
+    }
+}
