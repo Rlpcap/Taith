9c504e646529faa53898f9d7ba947b3af18d186e
Florencia Fernández <apoolf@gmail.com>
2020-10-28 20:48:24 -0300

    Menu & Landing

--- Assets/Scripts/LandingController.cs
+++ Assets/Scripts/LandingController.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LandingController : MonoBehaviour
 {
+    public GameObject fade;
+    bool _pressedKey = false;
+
     void Update()
     {
-        if (Input.anyKeyDown)
-            SceneManager.LoadScene("Menu");
+        if (Input.anyKeyDown && !_pressedKey)
+        {
+            StartCoroutine(OutAnim());
+            _pressedKey = true;
+        }
+    }
+
+    IEnumerator OutAnim()
+    {
+        fade.GetComponent<Animator>().SetTrigger("out");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("Menu");
     }
 }
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject fade;
+
+
     void Start()
     {
+        StartCoroutine(OutAnim());
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator OutAnim()
+    {
+        yield return new WaitForSeconds(1.5f);
+        fade.SetActive(false);
     }
 }
