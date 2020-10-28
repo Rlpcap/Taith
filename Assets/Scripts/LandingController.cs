a689d98c751dcc7e9d7f73ab3a3ff2de14643a10
Rlpcap <rafael.lopezpisani@davinci.edu.ar>
2020-10-28 20:22:42 -0300

    LandingController

---
+++ Assets/Scripts/LandingController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LandingController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKeyDown)
+            SceneManager.LoadScene("Menu");
+    }
+}
