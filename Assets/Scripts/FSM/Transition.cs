6301c816f05ca5b7b270d77e10010db5134085a7
Rlpcap <rafael.lopezpisani@davinci.edu.ar>
2021-03-30 15:43:50 -0300

    +Agregados scripts FSM(no implementados)

---
+++ Assets/Scripts/FSM/Transition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyFSM
+{
+	public class Transition<T>
+    {
+		public T Input { get { return input; } }
+		public State<T> TargetState { get { return targetState;  } }
+
+		T input;
+		State<T> targetState;
+
+		public Transition(T input, State<T> targetState)
+        {
+			this.input = input;
+			this.targetState = targetState;
+		}
+	}
+}
