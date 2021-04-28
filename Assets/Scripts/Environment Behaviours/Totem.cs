5d4262f5c505fe84f4351e2503608681c812752b
Florencia Fernández <apoolf@gmail.com>
2021-04-27 21:55:33 -0300

    Totem y cambios en carpetas.

--- Assets/Scripts/Environment Behaviours/Totem.cs
+++ Assets/Scripts/Environment Behaviours/Totem.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Totem : MonoBehaviour
 {
     public List<TotemPlatform> linkedPlatforms = new List<TotemPlatform>();
+    Animator _anim;
+    public Material _mat;
+
+    public void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+        _mat.SetColor("_EmissionColor", Color.white * 5);
+    }
 
     private void OnTriggerEnter(Collider coll)
     {
         if (coll.GetComponent<PlayerModel>())
         {
+            _mat.SetColor("_EmissionColor", Color.green * 5);
+            _anim.SetTrigger("pressed");
             foreach (var item in linkedPlatforms)
             {
                 item.Inactive = false;
@@ -21,6 +31,8 @@ public class Totem : MonoBehaviour
     {
         if (coll.GetComponent<PlayerModel>())
         {
+            _mat.SetColor("_EmissionColor", Color.white * 5);
+            _anim.SetTrigger("exit");
             foreach (var item in linkedPlatforms)
             {
                 item.Inactive = true;
