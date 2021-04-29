742ff7cbd6c49e34125e33321066fbbd9df9cef7
Rlpcap <rafael.lopezpisani@davinci.edu.ar>
2021-04-29 20:07:24 -0300

    Cambios para hacer funcionar al GameManager

---
+++ Assets/Scripts/Game/EndLevelPortal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelPortal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider coll)
+    {
+        var pl = coll.gameObject.GetComponent<PlayerModel>();
+        if (pl)
+            GameManager.Instance.lastLevelAchieved++;
+    }
+}
---
+++ Assets/Scripts/Game/LobbyPortal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyPortal : MonoBehaviour
+{
+    public int myLobbySpawnIndex;
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        var pl = coll.gameObject.GetComponent<PlayerModel>();
+
+        if (pl)
+            GameManager.Instance.lobbySpawnIndex = myLobbySpawnIndex;
+    }
+}
