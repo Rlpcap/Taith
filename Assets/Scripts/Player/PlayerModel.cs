56172f84f4399489bfc60332e267eaf7040df39e
Rlpcap <rafael.lopezpisani@davinci.edu.ar>
2021-05-02 21:25:52 -0300

    Pequeños cambios en gameobjects de levelintro. Sonido de salto ahora no suena en relacion al espacio.

--- Assets/Scripts/Player/PlayerModel.cs
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -226,7 +226,7 @@ public class PlayerModel : MonoBehaviour, IUpdate, IFreezable
         if (_currentJumps > 0 && _canMove)
         {
             onJump(_grounded);
-            SoundManager.PlaySound(SoundManager.Sound.PlayerJump,transform.position);
+            SoundManager.PlaySound(SoundManager.Sound.PlayerJump);
 
             _checkGround = false;
             //_velocity = Vector3.zero;
