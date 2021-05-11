ce1058102ecc01a614d0f5387ffb2a0ae2e558d6
julianbernabeu <julian.bernabeu@davinci.edu.ar>
2021-05-11 00:50:37 -0300

    Interacción npc Ventana de dialogo
    -Hecha ventana de dialogo para que al atravesar un trigger, aparezca un texto reproducido.

---
+++ Assets/Scripts/Npcs/DialogueWindow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueWindow : MonoBehaviour
+{
+    const string kAlphaCode = "<color=#00000000>";
+    const float kMaxTextTime = 0.1f;
+
+    public static int textSpeed = 2;
+    public Text dialogueText;
+
+    string _currentText;
+    CanvasGroup _canvasGroup;
+
+    private void Awake()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void Start()
+    {
+        _canvasGroup.alpha = 0;
+    }
+
+    public void ShowText(string text)
+    {
+        _canvasGroup.alpha = 1;
+        _currentText = text;
+        StartCoroutine(DisplayText());
+    }
+
+    public void Close()
+    {
+        StopAllCoroutines();
+        _canvasGroup.alpha = 0;
+    }
+
+    IEnumerator DisplayText()
+    {
+        dialogueText.text = "";
+
+        foreach (var c in _currentText.ToCharArray())
+        {
+            dialogueText.text += c;
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/Npcs/TriggerDialogueBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDialogueBox : MonoBehaviour
+{
+    [TextArea]
+    public string dialogueText;
+
+    public DialogueWindow dialogueWindow;
+
+    int _player = 8;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == _player)
+        {
+            Debug.Log("TRIGGER TEXT!");
+            dialogueWindow.ShowText(dialogueText);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.layer==_player)
+        {
+            dialogueWindow.Close();
+        }
+    }
+}
