31e1c14bf56b5fdd99e98d665aefedc9c0bf01f6
julianbernabeu <julian.bernabeu@davinci.edu.ar>
2021-06-10 17:33:02 -0300

    Quest Manager
    Añadido Sistema de quests(Solo los scripts)

---
+++ Assets/Scripts/Game/Quest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Quest
+{
+    public string QuestName;
+    public QuestState.State QuestStatus;
+
+    public Dictionary<string,bool> tasks = new Dictionary<string, bool>();
+
+
+}
