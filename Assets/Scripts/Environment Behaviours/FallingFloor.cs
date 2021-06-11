07aa2eb3d397de3b51e7a2d59df1adc9da837b3d
Rlpcap <rafael.lopezpisani@davinci.edu.ar>
2021-06-11 13:28:25 -0300

    Agregada interfaz de ICorrupt para futuro. Cambios pequeños level intro

--- Assets/Scripts/Environment Behaviours/FallingFloor.cs
+++ Assets/Scripts/Environment Behaviours/FallingFloor.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FallingFloor : FallingObject, IIce, IMud
+public class FallingFloor : FallingObject, IIce, IMud, ICorrupt
 {
     public FallingBridgeRB bridge;
 
