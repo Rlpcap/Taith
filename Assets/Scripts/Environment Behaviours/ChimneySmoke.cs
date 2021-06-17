107f6eae8cd6c510a46e40c146cc573069975ca8
Rlpcap <rafael.lopezpisani@davinci.edu.ar>
2021-06-17 20:33:42 -0300

    *Cambios detalles chimenea y cascada

---
+++ Assets/Scripts/Environment Behaviours/ChimneySmoke.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChimneySmoke : MonoBehaviour
+{
+    public float setOnFireDuration;
+    private void OnTriggerEnter(Collider coll)
+    {
+        var pl = coll.gameObject.GetComponent<PlayerModel>();
+
+        if (pl)
+        {
+            if (!pl.Shielded)
+                pl.SetOnFire(setOnFireDuration);
+        }
+    }
+}
---
+++ Assets/Scripts/Environment Behaviours/WaterCurrent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCurrent : MonoBehaviour
+{
+    public float force;
+
+    private void OnTriggerStay(Collider coll)
+    {
+        var pl = coll.GetComponent<PlayerModel>();
+        if(pl)
+            pl.GetComponent<Rigidbody>().AddForce(transform.forward * force, ForceMode.Acceleration);
+    }
+}
