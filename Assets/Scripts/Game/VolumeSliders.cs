5879cba08e4739d2f8e8eefcf581e50062144ce5
Rlpcap <rafael.lopezpisani@davinci.edu.ar>
2021-07-06 20:47:33 -0300

    Arreglo de sliders de sonidos.

--- Assets/Scripts/Game/VolumeSliders.cs
+++ Assets/Scripts/Game/VolumeSliders.cs
@@ -9,21 +9,21 @@ public class VolumeSliders : MonoBehaviour
 
     public void SetMasterVolume(float vol)
     {
-        audioMixer.SetFloat("MasterVol", vol);
+        audioMixer.SetFloat("MasterVol", Mathf.Log10(vol) * 20);
     }
 
     public void SetMusicVolume(float vol)
     {
-        audioMixer.SetFloat("MusicVol", vol);
+        audioMixer.SetFloat("MusicVol", Mathf.Log10(vol) * 20);
     }
 
     public void SetSoundEffectsVolume(float vol)
     {
-        audioMixer.SetFloat("SoundEffectsVol", vol);
+        audioMixer.SetFloat("SoundEffectsVol", Mathf.Log10(vol) * 20);
     }
 
     public void SetEnvironmentVolume(float vol)
     {
-        audioMixer.SetFloat("EnvironmentVol", vol);
+        audioMixer.SetFloat("EnvironmentVol", Mathf.Log10(vol) * 20);
     }
 }
