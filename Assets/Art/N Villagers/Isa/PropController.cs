420632d837bf87d144191ac09f7757ad14a0aef5
Florencia Fernández <apoolf@gmail.com>
2022-02-02 14:47:44 -0300

    Nuevo NPC y Corrección de Errores
    + Árbol flotando arreglado.
    + Layer Ground a Assets faltantes.
    + Nuevo NPC (Bibliotecaria).

---
+++ Assets/Art/N Villagers/Isa/PropController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropController : MonoBehaviour
+{
+    public GameObject prop;
+    public string triggerName;
+
+    public void ActiveAnim()
+    {
+        prop.GetComponent<Animator>().SetTrigger(triggerName);
+    }
+}
