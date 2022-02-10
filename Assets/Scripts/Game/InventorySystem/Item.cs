ed09fca1d14cf8f2fd9b9fa25c5ae9ec87cf3af3
julianbernabeu <julian.bernabeu@davinci.edu.ar>
2022-02-09 21:16:34 -0300

    Añadido JSON para guardar items
    _Ahora podemos guardar la data. Esto lo neceisot para despues integrarlo al sistema de quests
    _lo siguiente es terminar el sistema de inventario, para asi poder terminar el sistema de quests.

---
+++ Assets/Scripts/Game/InventorySystem/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public string itemName;
+
+    public bool itemModifier;
+
+    public Item(string _itemName, bool _itemModifier)
+    {
+        this.itemName = _itemName;
+        this.itemModifier = _itemModifier;
+    }
+}
---
+++ Assets/Scripts/Game/InventorySystem/ItemDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class ItemDatabase : Singleton<ItemDatabase>
+{
+    private List<Item> _items = new List<Item>();
+    void Start()
+    {
+
+        _items.Add(new Item("Scroll", false));
+        SaveDataBase();
+        BuildDatabase();
+    }
+
+    public void SaveDataBase()
+    {
+        string json = JsonUtility.ToJson(_items[0]);
+
+        File.WriteAllText(Application.dataPath + "/Resources/JSON/Items.json", json);
+
+        _items[0] = JsonUtility.FromJson<Item>(json);
+
+        //TENGO QUE VER COMO PUEDO HACER PARA GUARDAR SIN USAR LISTAS
+    }
+    private void BuildDatabase()
+    {
+        //deserializar items
+
+        Debug.Log(_items[0].itemName);
+        Debug.Log(_items[0].itemModifier);
+
+
+    }
+
+}
