6cfaf1669502d99a923f292a87a8e5c08f5326ef
julianbernabeu <julian.bernabeu@davinci.edu.ar>
2022-02-22 23:45:34 -0300

    Agregado item de cantidad al sistema de inventario+ un prefab de placeholder para las monedas

--- Assets/Scripts/Game/InventorySystem/InventoryController.cs
+++ Assets/Scripts/Game/InventorySystem/InventoryController.cs
@@ -5,18 +5,45 @@ using UnityEngine;
 public class InventoryController : Singleton<InventoryController>
 {
     public List<Item> playerItems = new List<Item>();
-    
+
 
     public void GiveItem(string itemName)
     {
         Item item = ItemDatabase.Instance.GetItem(itemName);
-        playerItems.Add(item);
-        Debug.Log(playerItems.Count + " items in inventory. Added item: " + itemName);
+        CheckItemType(item);
+
         foreach (var i in playerItems)
         {
             Debug.Log(i.itemName);
         }
         UIEventHandler.ItemAddedToInventory(item);
+    }
+
+    void CheckItemType(Item item)
+    {
+        if (item.itemType == Item.ItemTypes.QuestItem)
+        {
+            playerItems.Add(item);
+            Debug.Log(playerItems.Count + " items in inventory. Added item: " + item.itemName);
+            return;
+        }
+
+        if (item.itemType == Item.ItemTypes.NormalItem)
+        {
+            foreach (var i in playerItems)
+            {
+                if (item.itemName == i.itemName)
+                {
+                    item.ammount++;
+                    Debug.Log(playerItems.Count + " items in inventory. Added item: " + item.itemName + ". " + "Number of " + item.itemName + " in total: " + item.ammount);
+                    return;
+                }
+            }
+            playerItems.Add(item);
+            item.ammount++;
+            Debug.Log(playerItems.Count + " items in inventory. Added item: " + item.itemName + ". " + "Number of " + item.itemName + " in total: " + item.ammount);
+        }
+
     }
 
 }
--- Assets/Scripts/Game/InventorySystem/Item.cs
+++ Assets/Scripts/Game/InventorySystem/Item.cs
@@ -8,16 +8,18 @@ public class Item
     public enum ItemTypes { QuestItem, NormalItem };
     public string itemName;
     public bool itemModifier;
+    public int ammount;
 
     [JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
     public ItemTypes itemType;
 
 
     [Newtonsoft.Json.JsonConstructor]
-    public Item(string _itemName, bool _itemModifier, ItemTypes _itemType)
+    public Item(string _itemName, bool _itemModifier, ItemTypes _itemType, int _ammount)
     {
         this.itemName = _itemName;
         this.itemModifier = _itemModifier;
         this.itemType = _itemType;
+        this.ammount = _ammount;
     }
 }
