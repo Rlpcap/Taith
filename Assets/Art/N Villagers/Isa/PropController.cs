5c7e53d84dff9915680645c8adb5c98940f4c87d
Florencia Fernández <apoolf@gmail.com>
2022-02-28 11:50:50 -0300

    Cambios en el level intro.
    + Anims de Quest aceleradas.
    + Book de Isa se baja con la anim de quest.
    + Colliders a los farmers.

--- Assets/Art/N Villagers/Isa/PropController.cs
+++ Assets/Art/N Villagers/Isa/PropController.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class PropController : MonoBehaviour
 {
     public GameObject prop;
+    public GameObject prop2;
     public string triggerName;
 
     public void ActiveAnim()
     {
         prop.GetComponent<Animator>().SetTrigger(triggerName);
+    }
+
+    public void BookDown()
+    {
+        prop2.GetComponent<Animator>().SetBool("quest", true);
+    }
+
+    public void BookUp()
+    {
+        prop2.GetComponent<Animator>().SetBool("quest", false);
     }
 }
