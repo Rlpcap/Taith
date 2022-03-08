aee540fc75d2b71d3f09584217df0bb8340a78f8
Rlpcap <rafael.lopezpisani@davinci.edu.ar>
2022-03-08 17:09:40 -0300

    Sliders de sonido actualizados en el Start

--- Assets/Scripts/Game/VolumeSliders.cs
+++ Assets/Scripts/Game/VolumeSliders.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeSliders : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
+    public Slider masterVol, musicVol, sfxVol, environmentVol;
+
+    private void Start()
+    {
+        float sliderValue;
+        audioMixer.GetFloat("MasterVol", out sliderValue);
+        masterVol.value = Mathf.Pow(10, sliderValue / 20);
+
+        audioMixer.GetFloat("MusicVol", out sliderValue);
+        musicVol.value = Mathf.Pow(10, sliderValue / 20);
+
+        audioMixer.GetFloat("SoundEffectsVol", out sliderValue);
+        sfxVol.value = Mathf.Pow(10, sliderValue / 20);
+
+        audioMixer.GetFloat("EnvironmentVol", out sliderValue);
+        environmentVol.value = Mathf.Pow(10, sliderValue / 20);
+    }
+
     public void SetMasterVolume(float vol)
     {
         audioMixer.SetFloat("MasterVol", Mathf.Log10(vol) * 20);
-        Debug.Log(Mathf.Log10(vol) * 20);
     }
 
     public void SetMusicVolume(float vol)
