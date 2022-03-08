bd037f7dada83f898068f77664b3fb327186dc4c
Rlpcap <rafael.lopezpisani@davinci.edu.ar>
2022-03-08 19:22:18 -0300

    Coins en niveles

--- Assets/Scripts/NPC/NPC_NoQuest.cs
+++ Assets/Scripts/NPC/NPC_NoQuest.cs
@@ -7,13 +7,14 @@ public class NPC_NoQuest : NPC
 {
     public SoundManager.Sound voiceSound;
     public bool coinCounter = false;
+    public int maxCoins;
 
     public override void Start()
     {
         base.Start();
         if (coinCounter && InventoryController.Instance.playerItems.Any(i => i.itemName == "Coin"))
         {
-            defaultText = "Congratulations! You found " + InventoryController.Instance.playerItems.Where(i => i.itemName == "Coin").First().ammount + " out of x Coins!";
+            defaultText = "Congratulations! You found " + InventoryController.Instance.playerItems.Where(i => i.itemName == "Coin").First().ammount + " out of " + maxCoins + " Coins!";
         }
     }
 
@@ -27,7 +28,7 @@ public class NPC_NoQuest : NPC
 
         if (coinCounter && InventoryController.Instance.playerItems.Any(i => i.itemName == "Coin"))
         {
-            defaultText = "Congratulations! You found " + InventoryController.Instance.playerItems.Where(i => i.itemName == "Coin").First().ammount + " out of x Coins!";
+            defaultText = "Congratulations! You found " + InventoryController.Instance.playerItems.Where(i => i.itemName == "Coin").First().ammount + " out of " + maxCoins + " Coins!";
         }
 
         if (chatState == ChatState.Talking)
